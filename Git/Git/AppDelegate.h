b6a7e9d714e51f2d7fd8c84778471d16cac72e66
赵英奎 <zhaoyingkui@1.local>
2016-03-04 21:44:50 +0800

    这是第一次提交

---
+++ Git/Git/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Git
+//
+//  Created by 赵英奎 on 16/3/4.
+//  Copyright © 2016年 KK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
